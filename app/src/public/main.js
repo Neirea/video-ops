1cf6e00c878e3d6a7fc9c40f8a3ba6194cf517cb
neirea <neireasoultix@gmail.com>
2023-02-26 19:47:57 +0200

    hide not loaded preview image

--- app/src/public/main.js
+++ app/src/public/main.js
@@ -496,7 +496,8 @@ function handleTimelineUpdate(e) {
     const percent =
         Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width;
     //thumbnail image
-    const previewImgSrc = thumbnails[Math.floor(percent * 100)] || "";
+    const previewImgSrc = thumbnails[Math.floor(percent * 100)];
+    previewImg.style.display = previewImgSrc ? "block" : "none"; // don't show if not loaded yet
     previewImg.src = previewImgSrc;
     const previewX =
         e.x + previewImg.offsetWidth / 2 > rect.right

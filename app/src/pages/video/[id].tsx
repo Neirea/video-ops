b657de71fdc2d106825f6f81e7872b675703877c
neirea <neireasoultix@gmail.com>
2023-05-01 22:28:31 +0300

    copy button for embedded link

--- app/src/pages/video/[id].tsx
+++ app/src/pages/video/[id].tsx
@@ -18,9 +18,10 @@ const VideoPage = ({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const router = useRouter();
     const [origin, setOrigin] = useState("");
-    const videoId = router.query.id as string | undefined;
     const [videos, setVideos] = useState(videoNames);
+    const [copied, setCopied] = useState(false);
 
+    const videoId = router.query.id as string | undefined;
     const currentVideo = videoNames.find((i) => i.url == videoId);
 
     useEffect(() => {
@@ -51,6 +52,12 @@ const VideoPage = ({
         );
 
         setVideos(videoNames);
+    }
+    function handleCopy() {
+        if (!currentVideo) return;
+        navigator.clipboard.writeText(getIframeLink(origin, currentVideo.url));
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
     }
 
     if (!currentVideo || !videoId) return null;
@@ -87,6 +94,31 @@ const VideoPage = ({
                             >
                                 {getIframeLink(origin, currentVideo.url)}
                             </div>
+                            <button
+                                className="text-white relative"
+                                onClick={handleCopy}
+                            >
+                                {!copied ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="50.788"
+                                        height="50.723"
+                                        viewBox="0 0 50.788 50.723"
+                                        className="invert h-6 w-6"
+                                    >
+                                        <path
+                                            id="Copy-Icon-SVG-098567"
+                                            d="M8.213,49.723A8.222,8.222,0,0,1,0,41.511V17A8.222,8.222,0,0,1,8.213,8.787H32.724A8.222,8.222,0,0,1,40.936,17V41.511a8.222,8.222,0,0,1-8.212,8.213ZM4,17V41.511a4.217,4.217,0,0,0,4.214,4.212H32.724a4.217,4.217,0,0,0,4.212-4.212V17a4.218,4.218,0,0,0-4.212-4.213H8.213A4.218,4.218,0,0,0,4,17ZM41.575,36.936a4.219,4.219,0,0,0,4.214-4.213V11.245A7.254,7.254,0,0,0,38.543,4H17.064a4.217,4.217,0,0,0-4.212,4.214h-4A8.221,8.221,0,0,1,17.064,0H38.543A11.258,11.258,0,0,1,49.788,11.245V32.723a8.221,8.221,0,0,1-8.213,8.212Z"
+                                            transform="translate(0.5 0.5)"
+                                            stroke="rgba(0,0,0,0)"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <div className="h-6 w-6 before:absolute before:content-['Copied'] before:bg-stone-700 before:-top-6 before:right-0 before:z-50 before:px-1 before:py-0 before:rounded">
+                                        ✔
+                                    </div>
+                                )}
+                            </button>
                         </div>
                     )}
                 </div>

f8e4689b5d541bcffa71a34de9cf004b50d0d098
neirea <neireasoultix@gmail.com>
2023-05-04 15:10:20 +0300

    improved embedded getStaticProps

--- app/src/pages/embed/[id].tsx
+++ app/src/pages/embed/[id].tsx
@@ -2,28 +2,28 @@ import VideoPlayer from "@/components/video-player/VideoPlayer";
 import dbConnect from "@/lib/connect-db";
 import { Video, VideoType } from "@/models/Video";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { useRouter } from "next/router";
 
 const EmbedVideo = ({
-    videoNames,
+    video,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-    const router = useRouter();
-    const videoId = router.query.id as string | undefined;
-    const currentVideo = videoNames.find((i) => i.url == videoId);
-
-    if (!currentVideo || !videoId) return null;
-    return <VideoPlayer type="embed" video={currentVideo} />;
+    return <VideoPlayer type="embed" video={video} />;
 };
 
 export const getServerSideProps: GetServerSideProps<{
-    videoNames: VideoType[];
-}> = async (ctx) => {
+    video: VideoType;
+}> = async ({ query }) => {
     await dbConnect();
-    const videoNames = await Video.find({});
+    // list of videos
+    const videoUrl = query.id as string | undefined;
+    const video = await Video.findOne({ url: videoUrl });
 
+    if (!video) {
+        return {
+            notFound: true,
+        };
+    }
     return {
-        // workaround to serialize dates
-        props: { videoNames: JSON.parse(JSON.stringify(videoNames)) },
+        props: { video: JSON.parse(JSON.stringify(video)) },
     };
 };
 

2051372849c7800e3eeeaab67a22146bac6c35ca
Neirea <neireasoultix@gmail.com>
2023-06-19 01:12:10 +0300

    async queue retry mechanism

--- video-process-app/src/AsyncQueue.ts
+++ video-process-app/src/AsyncQueue.ts
@@ -10,17 +10,30 @@ export default class AsyncQueue<T extends Object> {
     private maxWorkers: number;
     private activeWorkers: number;
     private processFn: (job: Job<T>) => any;
+    private retryInterval: number;
+    private maxRetries: number;
 
     constructor(
         redisClient: RedisClientType<any, any, any>,
         queueName = "myQueue",
-        maxWorkers = 1
+        options: {
+            maxWorkers?: number;
+            retryInterval?: number;
+            maxRetries?: number;
+        } = {}
     ) {
+        const {
+            maxWorkers = 1,
+            retryInterval = 30000,
+            maxRetries = 3,
+        } = options;
         this.queueName = queueName;
         this.redisClient = redisClient;
         this.maxWorkers = maxWorkers;
         this.activeWorkers = 0;
         this.processFn = () => {};
+        this.retryInterval = retryInterval;
+        this.maxRetries = maxRetries;
     }
 
     public async add(args: T) {
@@ -33,22 +46,41 @@ export default class AsyncQueue<T extends Object> {
         this.processFn = processFn;
     }
 
-    private async processLoop(): Promise<boolean> {
+    private async processLoop(retryCount = 0): Promise<boolean> {
         //check if all workers are occupied;
         if (this.activeWorkers >= this.maxWorkers) {
             return false;
         }
-        this.activeWorkers++;
-        const taskData = await this.redisClient.lPop(this.queueName);
-        if (!taskData) {
+        try {
+            this.activeWorkers++;
+            const taskData = await this.redisClient.lPop(this.queueName);
+            if (!taskData) {
+                this.activeWorkers--;
+                return false;
+            }
+            const currentTaskArgs: T = JSON.parse(taskData);
+            await this.processFn({ data: currentTaskArgs });
+        } catch (error) {
+            console.error(error);
+            if (retryCount < this.maxRetries) {
+                // Retry after the specified interval
+                await sleep(this.retryInterval);
+                return this.processLoop(retryCount + 1);
+            } else {
+                // Max retries reached, give up on the task
+                console.error(`Max retries reached for task.`);
+            }
+        } finally {
             this.activeWorkers--;
-            return false;
+            await this.processLoop();
         }
-        const currentTaskArgs: T = JSON.parse(taskData);
-        await this.processFn({ data: currentTaskArgs });
-        this.activeWorkers--;
-        await this.processLoop();
 
         return true;
     }
+}
+
+function sleep(delay: number) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
+    });
 }

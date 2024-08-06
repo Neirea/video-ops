c6d8078b5523d12b299d063fba05bb451ad638fb
neirea <neirea@ukr.net>
2024-08-06 16:55:09 +0300

    update packages for client-app

--- client-app/next.config.mjs
+++ client-app/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ["@aws-sdk"],
+    },
     async redirects() {
         return [
             { source: "/", destination: "/video/default", permanent: true },

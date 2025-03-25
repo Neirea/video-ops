896b9fbed8ca71c75d3ff37949f79c5ae93121ee
neirea <neirea@ukr.net>
2025-03-25 02:17:27 +0200

    remove deprecated iframe property

--- client-app/src/utils/getIframeLink.ts
+++ client-app/src/utils/getIframeLink.ts
@@ -1,3 +1,3 @@
 export default function getIframeLink(origin: string, url: string) {
-    return ` <iframe src="${origin}/embed/${url}" frameborder="0" width="640" height="360"></iframe>`;
+    return ` <iframe src="${origin}/embed/${url}" width="640" height="360"></iframe>`;
 }

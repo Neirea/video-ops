f92d6ba583d68c6971ff93e12618e3c1fadead74
neirea <neirea@ukr.net>
2026-02-24 14:58:28 +0200

    update packages

--- client-app/eslint.config.js
+++ client-app/eslint.config.js
@@ -1,10 +1,11 @@
 // @ts-check
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
     {
         ignores: ["eslint.config.js"],
     },
